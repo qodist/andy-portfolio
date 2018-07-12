318335a6b5321f6cb36226fd7dbaac1be2e7bb84
qodist <reyno75@gmail.com>
2018-07-12 19:43:44 +0300

    nearly completed the contact form

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
+//FIRST TAB MENU
 function openSection (evt, sectionName){
     var i, tabcontent, tablinks;
 
-    tabcontent = document.getElementsByClassName("tabcontent");
+    tabcontent = document.getElementsByClassName("tab-menu__content");
     for(i = 0; i < tabcontent.length; i++){
         tabcontent[i].style.display = "none";
     }
-    tablinks = document.getElementsByClassName("tablinks");
+    tablinks = document.getElementsByClassName("tab-menu__links");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
@@ -14,4 +15,23 @@ function openSection (evt, sectionName){
 }
 
 // Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
+document.getElementById("defaultOpen-1").click();
+
+//SECOND TAB MENU
+function openSectionTwo (evt, sectionName){
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tab-menu__content--1");
+    for(i = 0; i < tabcontent.length; i++){
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tab-menu__links--1");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(sectionName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen-2").click();
